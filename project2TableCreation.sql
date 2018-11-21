3b1beb3dd6bce7065594df12dbc5c0be3fff3653
Nigel Christian <callmekurisu@gmail.com>
2018-11-21 15:29:19 -0600

    update db

--- project2TableCreation.sql
+++ project2TableCreation.sql
@@ -1,84 +1,85 @@
 /*******************************************************************************
    Create Tables
 ********************************************************************************/
+--DROP TABLE ANSWERS CASCADE;
+--DROP TABLE ANSWERSTATUS CASCADE;
+--DROP TABLE BOUNTIES CASCADE;
+--DROP TABLE BOUNTYSTATUS CASCADE;
+--DROP TABLE SUBJECTS CASCADE;
+--DROP TABLE USERROLES CASCADE;
+--DROP TABLE USERS CASCADE;
+--DROP TABLE WALLET CASCADE;
+
+CREATE TABLE AnswerStatus
+(
+	status_id 	NUMERIC     	PRIMARY KEY,
+	name  	VARCHAR(20) 	NOT NULL
+);
 
 CREATE TABLE Answers
 (
-    answerId  SERIAL 		    PRIMARY KEY,
-    description VARCHAR(511)	NOT NULL, 
-    submitted   TIMESTAMP 		NOT NULL,
-    votes 		INTEGER 		NOT NULL DEFAULT 0,
-    statusId 	INTEGER 		NOT NULL
+    answer_id  		SERIAL 		    PRIMARY KEY,
+    description 	VARCHAR(511)	NOT NULL, 
+    submitted   	TIMESTAMP 		NOT NULL,
+    votes 			INTEGER 		NOT NULL DEFAULT 0,
+    status_id 	INTEGER 			NOT NULL REFERENCES AnswerStatus (status_id),
+    bounty_id		INTEGER			
 );
 
-
-CREATE TABLE Bounties
+CREATE TABLE Roles
 (
-    bountyId  SERIAL 		    PRIMARY KEY,
-    description VARCHAR(511)	NOT NULL, 
-    submitted   TIMESTAMP 		NOT NULL,
-    amount 	    INTEGER			NOT NULL DEFAULT 0,
-    votes 		INTEGER 		NOT NULL DEFAULT 0,
-    timer	 	INTEGER			NOT NULL,
-    statusId 	INTEGER 		NOT NULL,
-	correctAnswer INTEGER	    REFERENCES Answers (answerId),
-	bountyPicture VARCHAR (150) 
+    role_id 	NUMERIC     PRIMARY KEY,
+	role_name  VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE Wallet
+(
+	wallet_id		INTEGER     PRIMARY KEY,
+	wallet_balance	INTEGER 	NOT NULL DEFAULT 0
+	
 );
 
 
 CREATE TABLE Users
 (
-    userId     SERIAL        PRIMARY KEY,
-    username   VARCHAR (50)  NOT NULL UNIQUE,
-    password   VARCHAR (50)  NOT NULL,
-	email      VARCHAR (150) NOT NULL UNIQUE,
-	picture	   VARCHAR (150),
-	roleId     INTEGER 		NOT NULL,
-	rating	   NUMERIC		NOT NULL DEFAULT 0,
-	wallet 	   INTEGER		NOT NULL DEFAULT 0	
+    user_id    	 	SERIAL        	PRIMARY KEY,
+    username   		VARCHAR (50)  	NOT NULL UNIQUE,
+    password   		VARCHAR (50)  	NOT NULL,
+	email      		VARCHAR (150) 	NOT NULL UNIQUE,
+	picture	   		VARCHAR (150),
+	rating	   		NUMERIC			NOT NULL DEFAULT 0,
+	wallet_id 	   	INTEGER			NOT NULL DEFAULT 0 REFERENCES Wallet (wallet_id),
+	role_id 		INTEGER 		NOT NULL REFERENCES Roles (role_id)
 );
 
 CREATE TABLE BountyStatus
 (
-    statusId NUMERIC     PRIMARY KEY,
-	status   VARCHAR(20) NOT NULL
-);   
+    bounty_status_id 	NUMERIC     	PRIMARY KEY,
+	status    			VARCHAR(20) 	NOT NULL
+);
 
 CREATE TABLE Subjects
 (
-    subjectId NUMERIC     PRIMARY KEY,
-	subjectType   VARCHAR(20) NOT NULL
+    subject_id 		NUMERIC     PRIMARY KEY,
+	subject_type   VARCHAR(20) 	NOT NULL
 );
 
-CREATE TABLE UserRoles
+CREATE TABLE Bounties
 (
-    userRoleId NUMERIC     PRIMARY KEY,
-	userRole   VARCHAR(10) NOT NULL
-);
+    bounty_id  			SERIAL 		    PRIMARY KEY,
+    description 		VARCHAR(511)	NOT NULL, 
+    submitted   		TIMESTAMP 		NOT NULL,
+    amount 	    		INTEGER			NOT NULL DEFAULT 0,
+    votes 				INTEGER 		NOT NULL DEFAULT 0,
+    timer	 			INTEGER			NOT NULL,
+    bounty_status_id 	INTEGER 		NOT NULL REFERENCES BountyStatus (bounty_status_id),
+	correct_answer 		INTEGER	    	REFERENCES 	Answers (answer_id),
+	bountyPicture 		VARCHAR (150), 
+	user_id 		 	INTEGER 		NOT NULL REFERENCES Users (user_id),
+	subject_id			INTEGER			NOT NULL REFERENCES Subjects (subject_id)
+	);
 
-CREATE TABLE Analytics
-(
-	totalAmount NUMERIC     PRIMARY KEY,
-	usersWithMoney  INTEGER NOT NULL DEFAULT 0
-);
 
-CREATE TABLE BountyToAnswers
-(
-	bountyToAnswerId  SERIAL    PRIMARY KEY,
-    bountyId 		  INTEGER   REFERENCES Bounties (bountyId),
-	answerId 		  INTEGER   REFERENCES Answers (answerId)
-);
 
-CREATE TABLE BountyToSubjects
-(
-	bountyToSubjectId  SERIAL    PRIMARY KEY,
-    bountyId 		  INTEGER   REFERENCES Bounties (bountyId),
-	subjectId 		  INTEGER   REFERENCES Subjects (subjectId)
-);
 
-CREATE TABLE UserToBounties
-(
-	userToBountyId	  SERIAL    PRIMARY KEY,
-	userId 		 	  INTEGER   REFERENCES Users (userId),
-    bountyId 		  INTEGER   REFERENCES Bounties (bountyId)
-);
+

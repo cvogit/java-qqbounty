772c2db5aaee5c8ddfaae500902f36b3c60ee0e6
Calvin Vo <cvogit@gmail.com>
2018-11-23 02:55:52 -0600

    Replaced spaces with tab

--- qqBounty_tables.sql
+++ qqBounty_tables.sql
@@ -1,28 +1,28 @@
 CREATE TABLE AnswerStatus
 (
-	answer_status_id 	NUMERIC     	PRIMARY KEY,
-	answer_status  		VARCHAR(20) 	NOT NULL
+    answer_status_id 	NUMERIC     	PRIMARY KEY,
+    answer_status  		VARCHAR(20) 	NOT NULL
 );
 
 CREATE TABLE Answers
 (
     answer_id  		SERIAL 		    PRIMARY KEY,
     description 	VARCHAR(511)	NOT NULL, 
-    submitted   	TIMESTAMP 		NOT NULL,
-    votes 			INTEGER 		NOT NULL DEFAULT 0,
-    status_id 		INTEGER 		NOT NULL REFERENCES AnswerStatus (answer_status_id)		
+    submitted   	TIMESTAMP 	    NOT NULL,
+    votes 			INTEGER 	    NOT NULL DEFAULT 0,
+    status_id 		INTEGER 	    NOT NULL REFERENCES AnswerStatus (answer_status_id)		
 );
 
 CREATE TABLE Roles
 (
     role_id 	NUMERIC     PRIMARY KEY,
-	role 	 	VARCHAR(10) NOT NULL
+    role 		VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE Wallet
 (
-	wallet_id		SERIAL     PRIMARY KEY,
-	balance			INTEGER 	NOT NULL DEFAULT 0
+    wallet_id		SERIAL     PRIMARY KEY,
+    balance			INTEGER    NOT NULL DEFAULT 0
 );
 
 CREATE TABLE Users

3c6c7f493c627424f11242aa9c94be2b85266aa3
Nigel Christian <callmekurisu@gmail.com>
2018-11-26 17:03:43 -0600

    update sql
    newest sql version, 11/26/2018 @ 1703

--- qqBounty_tables.sql
+++ qqBounty_tables.sql
@@ -1,11 +1,12 @@
--- DROP TABLE answers CASCADE;
--- DROP TABLE answerstatus CASCADE;
--- DROP TABLE bounties CASCADE;
--- DROP TABLE bountystatus CASCADE;
--- DROP TABLE roles CASCADE;
--- DROP TABLE subjects CASCADE;
--- DROP TABLE users CASCADE;
--- DROP TABLE wallets CASCADE;
+DROP TABLE answers CASCADE;
+DROP TABLE answerstatus CASCADE;
+DROP TABLE bounties CASCADE;
+DROP TABLE bountystatus CASCADE;
+DROP TABLE roles CASCADE;
+DROP TABLE subjects CASCADE;
+DROP TABLE users CASCADE;
+DROP TABLE wallets CASCADE;
+DROP TABLE SubjectsToBounties;
 
 CREATE TABLE AnswerStatus
 (
@@ -19,7 +20,8 @@ CREATE TABLE Answers
     description 	VARCHAR(511)	NOT NULL, 
     submitted   	TIMESTAMP 	    NOT NULL,
     votes			INTEGER 	    NOT NULL DEFAULT 0,
-    status_id 		INTEGER 	    NOT NULL REFERENCES AnswerStatus (answer_status_id)		
+    status_id 		INTEGER 	    NOT NULL REFERENCES AnswerStatus (answer_status_id),
+    bounty_id		INTEGER			NOT NULL 
 );
 
 CREATE TABLE Roles
@@ -67,27 +69,47 @@ CREATE TABLE Bounties
 	votes 				INTEGER 		NOT NULL DEFAULT 0,
 	timer	 			INTEGER			NOT NULL,
 	status_id 			INTEGER 		NOT NULL REFERENCES BountyStatus (bounty_status_id),
-	answer_id 			INTEGER	    	REFERENCES 	Answers (answer_id),
+	correct_answer_id 	INTEGER	    	REFERENCES 	Answers (answer_id),
 	picture 			VARCHAR (150), 
-	user_id 		 	INTEGER 		NOT NULL REFERENCES Users (user_id),
-	subject_id			INTEGER			NOT NULL REFERENCES Subjects (subject_id)
+	user_id 		 	INTEGER 		NOT NULL REFERENCES Users (user_id)
 );
 
 
+CREATE TABLE SubjectsToBounties
+(    
+    subjects_to_bounties_id	SERIAL 		PRIMARY KEY,
+    bounty_id				INTEGER		NOT NULL REFERENCES Bounties (bounty_id),
+    subject_id				INTEGER		NOT NULL REFERENCES Subjects (subject_id)
+);
 
+ALTER TABLE Answers ADD CONSTRAINT bounty_id_fk
+	FOREIGN KEY (bounty_id) REFERENCES Bounties (bounty_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 INSERT INTO qqbounty.roles (role_id, role) VALUES(1, 'user');
+
+INSERT INTO qqbounty.wallets
+(balance)
+VALUES(100);
+
+INSERT INTO qqbounty.users
+(username, "password", email, picture, rating, wallet_id, role_id)
+VALUES('testuser', 'password', 'email@mail.com', '', 0, 1, 1);
 
 
 INSERT INTO qqbounty.bountystatus (bounty_status_id , bounty_status) VALUES(1, 'posted');
 INSERT INTO qqbounty.bountystatus (bounty_status_id , bounty_status) VALUES(2, 'answered');
+INSERT INTO qqbounty.bountystatus (bounty_status_id , bounty_status) VALUES(3, 'expired');
 
 INSERT INTO qqbounty.subjects (subject_id , subject) VALUES(1, 'Math');
 INSERT INTO qqbounty.subjects (subject_id , subject) VALUES(2, 'Programming');
 
-INSERT INTO qqbounty.answerstatus (answer_status_id,answer_status ) VALUES(0, 'no correct answer');
 INSERT INTO qqbounty.answerstatus (answer_status_id,answer_status ) VALUES(1, 'submitted');
-INSERT INTO qqbounty.answerstatus (answer_status_id,answer_status ) VALUES(2, 'correct');
-INSERT INTO qqbounty.answerstatus (answer_status_id,answer_status ) VALUES(3, 'wrong');
+INSERT INTO qqbounty.answerstatus (answer_status_id,answer_status ) VALUES(2, 'reported');
 
-INSERT INTO qqbounty.answers (answer_id, description,submitted,votes,status_id) VALUES(0, 'No answer given yet',CURRENT_TIMESTAMP,0,0);
+INSERT INTO qqbounty.bounties
+(description, submitted, amount, votes, timer, status_id, correct_answer_id, picture, user_id)
+VALUES('test bounty', CURRENT_TIMESTAMP, 100, 0, 7000000, 1, null, '', 1);
+
+INSERT INTO qqbounty.answers (answer_id, description,submitted,votes,status_id, bounty_id) VALUES(1, 'No answer given yet',CURRENT_TIMESTAMP,0,1,1);
+
+SELECT * FROM ANSWERS;

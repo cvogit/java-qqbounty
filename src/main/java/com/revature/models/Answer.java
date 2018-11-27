09e1867e3958d6ac5aca7e56da76b013e8ed3c1a
Calvin Vo <cvogit@gmail.com>
2018-11-26 20:39:42 -0600

    Fixed Answer models fields and equals method

--- src/main/java/com/revature/models/Answer.java
+++ src/main/java/com/revature/models/Answer.java
@@ -18,156 +18,134 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int answer_id;
+	private int answerId;
 	
 	@NotNull
 	@Column
 	private String description;
 	
 	//Timestamp is generated via a utility, leave null on API call
-	@Nullable
+	@Null
 	private Timestamp submitted;
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private int answerId;
-		
-		
-		@NotNull
-		@Column
-		private String description;
-		
-		//Timestamp is generated via a utility, leave null on API call
-		@Null
-		private Timestamp submitted;
+	@NotNull
+	private int votes;
+	
+	@NotNull
+	private int statusId;
 
-		@NotNull
-		private int votes;
-		
-		@NotNull
-		private int statusId;
+	@NotNull
+	private int bountyId;
+	
+	
+	public Answer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-		@NotNull
-		private int bountyId;
-		
-		
-		public Answer() {
-			super();
-			// TODO Auto-generated constructor stub
-		}
+	public Answer(int answerId, @NotNull String description, Timestamp submitted, @NotNull int votes,
+			@NotNull int statusId, @NotNull int bountyId) {
+		super();
+		this.answerId = answerId;
+		this.description = description;
+		this.submitted = submitted;
+		this.votes = votes;
+		this.statusId = statusId;
+		this.bountyId = bountyId;
+	}
 
-		public Answer(int answerId, @NotNull String description, Timestamp submitted, @NotNull int votes,
-				@NotNull int statusId, @NotNull int bountyId) {
-			super();
-			this.answerId = answerId;
-			this.description = description;
-			this.submitted = submitted;
-			this.votes = votes;
-			this.statusId = statusId;
-			this.bountyId = bountyId;
-		}
+	public int getAnswerId() {
+		return answerId;
+	}
 
-		public int getAnswerId() {
-			return answerId;
-		}
-
-		public void setAnswerId(int answerId) {
-			this.answerId = answerId;
-		}
+	public void setAnswerId(int answerId) {
+		this.answerId = answerId;
+	}
 
 
-		public void setDescription(String description) {
-			this.description = description;
-		}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-		public Timestamp getSubmitted() {
-			return submitted;
-		}
+	public Timestamp getSubmitted() {
+		return submitted;
+	}
 
-		public void setSubmitted(Timestamp submitted) {
-			this.submitted = submitted;
-		}
+	public void setSubmitted(Timestamp submitted) {
+		this.submitted = submitted;
+	}
 
-		public int getVotes() {
-			return votes;
-		}
+	public int getVotes() {
+		return votes;
+	}
 
-		public void setVotes(int votes) {
-			this.votes = votes;
-		}
+	public void setVotes(int votes) {
+		this.votes = votes;
+	}
 
-		public int getStatusId() {
-			return statusId;
-		}
+	public int getStatusId() {
+		return statusId;
+	}
 
-		public void setStatusId(int statusId) {
-			this.statusId = statusId;
-		}
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
 
-		public int getBountyId() {
-			return bountyId;
-		}
+	public int getBountyId() {
+		return bountyId;
+	}
 
-		public void setBountyId(int bountyId) {
-			this.bountyId = bountyId;
-		}
+	public void setBountyId(int bountyId) {
+		this.bountyId = bountyId;
+	}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + answerId;
-			result = prime * result + bountyId;
-			result = prime * result + ((description == null) ? 0 : description.hashCode());
-			result = prime * result + statusId;
-			result = prime * result + ((submitted == null) ? 0 : submitted.hashCode());
-			result = prime * result + votes;
-			return result;
-		}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + answerId;
+		result = prime * result + bountyId;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + statusId;
+		result = prime * result + ((submitted == null) ? 0 : submitted.hashCode());
+		result = prime * result + votes;
+		return result;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Answer other = (Answer) obj;
-			if (answerId != other.answerId)
-				return false;
-			if (bountyId != other.bountyId)
-				return false;
-			if (description == null) {
-				if (other.description != null)
-					return false;
-			} else if (!description.equals(other.description))
-				return false;
-			if (statusId != other.statusId)
-				return false;
-			if (submitted == null) {
-				if (other.submitted != null)
-					return false;
-			} else if (!submitted.equals(other.submitted))
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		if (answerId != other.answerId)
+			return false;
+		if (bountyId != other.bountyId)
+			return false;
+		if (description == null) {
+			if (other.description != null)
 				return false;
 		} else if (!description.equals(other.description))
 			return false;
-		if (status_id != other.status_id)
+		if (statusId != other.statusId)
 			return false;
 		if (submitted == null) {
 			if (other.submitted != null)
 				return false;
-			return true;
-		}
+		} else if (!submitted.equals(other.submitted))
+			return false;
+		if (votes != other.votes)
+			return false;
+		return true;
+	}
 
-		@Override
-		public String toString() {
-			return "Answer [answerId=" + answerId + ", description=" + description + ", submitted=" + submitted
-					+ ", votes=" + votes + ", statusId=" + statusId + ", bountyId=" + bountyId + "]";
-		}
-		
-		
-
-
+	@Override
+	public String toString() {
+		return "Answer [answerId=" + answerId + ", description=" + description + ", submitted=" + submitted
+				+ ", votes=" + votes + ", statusId=" + statusId + ", bountyId=" + bountyId + "]";
+	}
 }
